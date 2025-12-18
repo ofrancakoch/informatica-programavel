1c2b5094de41cfaf937a93c2b0ac40fc30ef689c
Octavio Franca <ofrancakoch@gmail.com>
2025-12-18 19:23:01 -0300

    News Ticker Increment

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import logo_sentinelas from "./logo_sentinelas.png"
+import nome_sentinelas from "./nome_sentinelas.png"
 
 const Header = () => {
   return (
@@ -17,23 +18,39 @@ const NavBar = () => {
 
   return(
       <>
-        <nav className="flex items-center justify-between text-black md:px-50 bg-[#1976B9] drop-shadow-md">
-          <a href="#">
-            <img
-              src={logo_sentinelas}
-              alt="Logo Sentinelas"
-              className="w-[200px] hover:scale-105 transition-all py-3 pl-10 mx-auto"/>
-          </a>
-
-          <ul className="hidden xl:flex item-center gap-[6vw] font-semibold text-base py-6 pr-[10vw]">
+        <div className="w-full h-[10x] overflow-hidden whitespace-nowra bg-black">
+            <span className="flex items-center py-[1px] animate-infinite-scroll font-long-shot text-[#FFFF] text-2xl my-1">SISTEMA DE GERENCIAMENTO DE CLUBE</span>
+        </div>
+        <nav className=" flex item-center justify-center text-black md:px-50 bg-[#1976B9] drop-shadow-md">
+          <div className="flex items-center">
+            <a href="#">
+              <img
+                src={logo_sentinelas}
+                alt="Logo Sentinelas"
+                className="w-[150px] hover:scale-105 transition-all my-3"/>
+            </a>
+          </div>
+          {/*<ul className="hidden xl:flex item-center gap-[6vw] font-semibold text-base py-6 pr-[10vw]">
             <li className="p-3 hover:bg-sky-400 hover:text-white rounded-md transition-all cursor-pointer">Secretaria</li>
             <li className="p-3 hover:bg-sky-400 hover:text-white rounded-md transition-all cursor-pointer">Finances</li>
             <li className="p-3 hover:bg-sky-400 hover:text-white rounded-md transition-all cursor-pointer">Sobre</li>
             <li className="p-3 hover:bg-sky-400 hover:text-white rounded-md transition-all cursor-pointer">Entrar</li>
-          </ul>
-
+          </ul>*/}
+          
+          {/*<div className="flex items-center">
+            <i className="bx bx-search-alt color-white"></i>
+          </div>*/}
         </nav>
-        <hr className="border-solid border-[#FBC71F] border-2"></hr>
+        <hr className="border-solid border-[#FBC71F] border-4"></hr>
+    </>
+
+  );
+}
+
+
+const NewsTicker = () => {
+  return(
+    <>
     </>
 
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'long-shot': ["Long Shot Regular", "sans-serif"],
+      },
+      keyframes: {
+        "infinite-scroll": {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-19%)" },
+        },
+      },
+      animation: {
+        "infinite-scroll": "infinite-scroll 10s linear infinite"
+      },
+    },
   },
   plugins: [],
 }
